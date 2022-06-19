d963013420a5cb4554bfb30a53b4f713daee39b8
tvbayu2 <tvbayu2@gmail.com>
2022-06-19 17:56:16 +0800

    before video no 71

--- blogapp-course/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ blogapp-course/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,20 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "blogappcourse": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "blogappcoursed6a64a52": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
